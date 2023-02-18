747870ce24c8f0a98eb70eb9fc7d93f6a142d103
Navify97 <navneetji97@gmail.com>
2023-02-19 02:19:15 +0530

    first update

--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,16 @@
 package com.driver;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class RWOnly {
-    
+     private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
